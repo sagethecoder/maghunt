e073d9673083c16e5a6b42ae2f8cebc7e0d65a5d
sagethecoder <162547892+sagethecoder@users.noreply.github.com>
2024-03-15 17:12:26 +0530

    Add files via upload

---
+++ controllers/magnetController.js
@@ -0,0 +1,78 @@
+const Controller = require("./controller");
+const torren1337 = require("../services/1337x");
+const bitSearch = require("../services/bitSearch");
+const torLock = require("../services/torlock");
+const torProject = require("../services/torproject");
+
+class MagnetController extends Controller {
+   constructor(response) {
+      super(response);
+   }
+
+   async search(request) {
+      const { search } = request.query;
+
+      try {
+         //  const torren1337Results = torren1337(search);
+         //  const bitSearchResults = bitSearch(search);
+
+         //  const [torren1337Data, bitSearchData] = await Promise.all([torren1337Results, bitSearchResults]);
+
+         //  const combinedResults = {
+         //     torren1337: torren1337Data,
+         //     bitSearch: bitSearchData,
+         //  };
+         //  this.sendResponse(combinedResults);
+
+         const [torren1337Results, bitSearchResults, torResults, torProjectResults] = await Promise.all([
+            torren1337(search).catch((error) => {
+               console.error("Error from torren1337:", error);
+               return [];
+            }),
+            bitSearch(search).catch((error) => {
+               console.error("Error from bitSearch:", error);
+               return [];
+            }),
+            torLock(search).catch((error) => {
+               console.error("Error from bitSearch:", error);
+               return [];
+            }),
+            torProject(search).catch((error) => {
+               console.error("Error from bitSearch:", error);
+               return [];
+            }),
+         ]);
+
+         const validTorren1337Results = torren1337Results?.filter((result) => {
+            return result.Magnet && result.Magnet.startsWith("magnet:");
+         });
+
+         const validtorProjectResults = torProjectResults?.filter((result) => {
+            return result.Magnet && result.Magnet.startsWith("magnet:");
+         });
+         const validtortorResults = torResults?.filter((result) => {
+            return result.Magnet && result.Magnet.startsWith("magnet:");
+         });
+         const validbitSearchResults = bitSearchResults?.filter((result) => {
+            return result.Magnet && result.Magnet.startsWith("magnet:");
+         });
+
+         const combinedResults = [
+            ...(validTorren1337Results || []),
+            ...(validtortorResults || []),
+            ...(validbitSearchResults || []),
+            ...(validtorProjectResults || []),
+         ];
+
+         combinedResults.sort((a, b) => b.seeders - a.seeders);
+
+         const top30Results = combinedResults.slice(0, 20);
+
+         this.sendResponse(top30Results);
+      } catch (error) {
+         console.error(error);
+      }
+   }
+}
+
+module.exports = MagnetController;
